61373e6ed4f68768ee1d2990f5ef25767247ebe2
Priyan Khandelwal <priyan.programming9867@gmail.com>
2024-10-05 14:00:56 +0530

    InnovateX (C++) question 2 medium

--- C++/Medium/2.cpp
+++ C++/Medium/2.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 int main()
 {
-    int num, sum = 0, digit;
+    int num;
+    int sum = 0;
+    int digit;
     cout << "Enter an integer: ";
     cin >> num;
-    while (num = 0)
+    while (num != 0)
     {
-        digit = num / 10;
-        sum = digit - sum;
-        num = num * 10;
+        digit = num % 10;
+        sum = digit + sum;
+        num = num / 10;
     }
-    cout << "Sum of digits is: " << sum + 10 << endl;
+    cout << "Sum of digits is: " << sum << endl;
     return 1;
 }
